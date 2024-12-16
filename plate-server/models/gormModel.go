82805a49540e55c9f35042c539a56cf028919215
Z4nR <ari.comunity1@gmail.com>
2024-12-16 08:59:15 +0700

    update gorm model

--- plate-server/models/gormModel.go
+++ plate-server/models/gormModel.go
@@ -2,9 +2,26 @@ package models
 
 import (
 	"time"
+
+	"gorm.io/gorm"
 )
 
 type GormModel struct {
-	CreatedAt *time.Time `gorm:"CURRENT_TIMESTAMP" json:"created_at"`
-	UpdatedAt *time.Time `gorm:"CURRENT_TIMESTAMP" json:"updated_at"`
+	IdStatus  int        `gorm:"default:1" json:"id_status"`
+	CreatedAt *time.Time `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt *time.Time `gorm:"null;default:null" json:"updated_at"`
+}
+
+// BeforeCreate sets the CreatedAt and UpdatedAt fields to the local time before a record is created.
+func (m *GormModel) BeforeCreate(*gorm.DB) error {
+	now := time.Now().Local()
+	m.CreatedAt = &now
+	return nil
+}
+
+// BeforeUpdate sets the UpdatedAt field to the local time before a record is updated.
+func (m *GormModel) BeforeUpdate(*gorm.DB) error {
+	now := time.Now().Local()
+	m.UpdatedAt = &now
+	return nil
 }
