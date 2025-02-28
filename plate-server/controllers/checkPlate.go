7ed1c77ed5d92179d6984b2009176c5cd9302c17
Z4nR <ari.comunity1@gmail.com>
2025-03-01 03:51:13 +0700

    update check plate func

--- plate-server/controllers/checkPlate.go
+++ plate-server/controllers/checkPlate.go
@@ -8,6 +8,26 @@ type DataCode struct {
 	RegisterLastCode  *string `json:"lastCodeRegister"`
 }
 
-func CheckCode(ctx *gin.Context) {
+// Check Vehicle Type or Status by Plate Color
+func CheckVehicleStatus(ctx *gin.Context) {
 
+}
+
+// Check Vehicle Region
+func CheckVehicleRegion(ctx *gin.Context) {
+
+}
+
+// Check Vehicle Register Area
+func CheckVehicleRegisterArea(ctx *gin.Context) {
+
+}
+
+// Check Plate Data
+func CheckPlateData(ctx *gin.Context) {
+	// Check Vehicle Status
+	// Check Region Area
+	// Get Code Position
+	// Check Register Area
+	// Return Possibility Match Data About The Plate
 }
