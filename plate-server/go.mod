3fe4e0e817a4cf122292c3e90ebe5ae7e48db5f8
Z4nR <ari.comunity1@gmail.com>
2025-03-02 14:40:12 +0700

    chore: update go mod and utils

--- plate-server/go.mod
+++ plate-server/go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/gin-gonic/gin v1.10.0
 	github.com/google/uuid v1.6.0
 	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.10.6
 	golang.org/x/crypto v0.23.0
 	gorm.io/driver/postgres v1.5.7
 	gorm.io/gorm v1.25.10
--- plate-server/utils/customType.go
+++ plate-server/utils/customType.go
@@ -2,18 +2,22 @@ package utils
 
 import (
 	"database/sql/driver"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"strings"
 )
 
-// StringArray represents a custom type for []string.
-type StringArray []string
+// StringArrayDB represents a custom type for []string.
+type StringArrayDB []string
 
-// Scan implements the Scanner interface to read PostgreSQL array data into StringArray.
-func (s *StringArray) Scan(value interface{}) error {
+// StringArrayResponse is a custom type for handling JSON conversion of []string
+type StringArrayResponse []string
+
+// Scan implements the Scanner interface to read PostgreSQL array data into StringArrayDB.
+func (s *StringArrayDB) Scan(value interface{}) error {
 	if value == nil {
-		*s = StringArray{}
+		*s = StringArrayDB{}
 		return nil
 	}
 
@@ -34,17 +38,48 @@ func (s *StringArray) Scan(value interface{}) error {
 		// Convert []byte to string and call Scan again
 		return s.Scan(string(v))
 	default:
-		return errors.New("unsupported type for StringArray")
+		return errors.New("unsupported type for StringArrayDB")
 	}
 	return nil
 }
 
-// Value implements the Valuer interface to convert StringArray to the PostgreSQL format.
-func (s StringArray) Value() (driver.Value, error) {
+// Value implements the Valuer interface to convert StringArrayDB to the PostgreSQL format.
+func (s StringArrayDB) Value() (driver.Value, error) {
 	// Convert the []string array to a PostgreSQL array format: {value1,value2,value3}
 	if len(s) == 0 {
 		return nil, nil
 	}
 	// Create a comma-separated string enclosed in curly braces
 	return fmt.Sprintf("{%s}", strings.Join(s, ",")), nil
+}
+
+// Convert StringArrayResponse
+//
+//	to JSON for database storage
+func (s StringArrayResponse) Value() (driver.Value, error) {
+	return json.Marshal(s)
+}
+
+// Convert JSON from the database back to StringArrayResponse
+
+func (s *StringArrayResponse) Scan(value interface{}) error {
+	if value == nil {
+		*s = []string{} // Return empty slice instead of null
+		return nil
+	}
+
+	switch v := value.(type) {
+	case string:
+		// If it's a JSON string, parse it
+		if strings.HasPrefix(v, "[") {
+			return json.Unmarshal([]byte(v), s)
+		}
+		// Otherwise, treat it as a single plain string
+		*s = strings.Split(v, ", ") // Convert "Kia, Mazda" → ["Kia", "Mazda"]
+		return nil
+	case []byte:
+		return json.Unmarshal(v, s)
+	default:
+		return errors.New("invalid type for StringArrayResponse")
+	}
 }
