d630af54d81d80d73355e13c9d3b343a35f2492f
Z4nR <ari.comunity1@gmail.com>
2025-09-25 05:53:39 +0700

    chore: change routes file from ts to tsx

---
+++ plate-client/src/routes/routes.tsx
@@ -0,0 +1,25 @@
+import { createBrowserRouter, Navigate } from 'react-router';
+import App from '@/App';
+import Dashboard from '@/pages/DashboardPages';
+import Region from '@/pages/region/RegionPages';
+
+export const router = createBrowserRouter([
+  {
+    path: '/PlateFrom',
+    Component: App,
+    children: [
+      {
+        index: true,
+        Component: () => <Navigate to="dashboard" replace />,
+      },
+      {
+        path: 'dashboard',
+        Component: Dashboard,
+      },
+      {
+        path: 'region',
+        Component: Region,
+      },
+    ],
+  },
+]);
