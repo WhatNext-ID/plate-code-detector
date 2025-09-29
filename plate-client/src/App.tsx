55625e39f2c3c5ce5dbefaca07d7305042c6666b
Z4nR <ari.comunity1@gmail.com>
2025-09-29 18:32:37 +0700

    chore: update app for implement query client

--- plate-client/src/App.tsx
+++ plate-client/src/App.tsx
@@ -1,5 +1,14 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import Layout from './components/layout/layout';
 
+const queryClient = new QueryClient();
+
 export default function App() {
-  return <Layout />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen />
+      <Layout />
+    </QueryClientProvider>
+  );
 }
