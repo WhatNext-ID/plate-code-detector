60d46be3976bac504c126d9aa2e569b85525ad69
Z4nR <ari.comunity1@gmail.com>
2026-09-15 10:29:13 +0700

    Fix migration success message formatting by removing the period

--- plate-server/main.go
+++ plate-server/main.go
@@ -11,7 +11,7 @@ func main() {
 	if len(os.Args) > 1 && os.Args[1] == "migrate" {
 		database.StartDB()
 		database.RunMigrations()
-		fmt.Println("Migrations completed successfully.")
+		fmt.Println("Migrations completed successfully")
 		return
 	}
 
